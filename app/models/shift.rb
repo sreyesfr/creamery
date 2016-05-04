d18067b0515fc8aaf90a45494a1e8e9e25b0307d
Sarah Reyes-Franco <sreyesfr@andrew.cmu.edu>
2016-05-04 03:01:43 -0400

    minor final tweaks

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -11,7 +11,7 @@ class Shift < ActiveRecord::Base
   # Validations
   validates_date :date, on_or_after: lambda { :assignment_starts }, on_or_before_message: "must be on or after the start of the assignment"
   validates_time :start_time
-  #validates_time :end_time, after: :start_time, allow_blank: true
+  validates_time :end_time, allow_blank: true #after: :start_time
   validate :assignment_must_be_current
 
   # Scopes
